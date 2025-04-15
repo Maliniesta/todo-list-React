f14bca1bb87a5455f59f752de2bdea828c63699a
DigitalMalikB <malikbenaziza300@gmail.com>
2025-04-16 00:29:55 +0200

    correction du title

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/Maliniesta/todo-list-React.git",
+  base: "/todo-list-React/",
   plugins: [react(), tailwindcss()],
 });
